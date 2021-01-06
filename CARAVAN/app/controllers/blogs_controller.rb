d52a1ea1bbb18e69e73bbf73975e54d6e235a640
xas09 <akane.morita11@gmail.com>
2021-01-06 10:13:24 +0000

    [finish]3-3

--- CARAVAN/app/controllers/blogs_controller.rb
+++ CARAVAN/app/controllers/blogs_controller.rb
@@ -4,10 +4,21 @@ class BlogsController < ApplicationController
 
   def new
   end
+  
+  def create
+    blog = Blog.new(blog_params)
+    blog.save
+    redirect_to blogs_path
+  end
 
   def show
   end
 
   def edit
+  end
+  
+  private
+  def blog_params
+    params.require( :blog).permit( :title, :category, :body)
   end
 end
